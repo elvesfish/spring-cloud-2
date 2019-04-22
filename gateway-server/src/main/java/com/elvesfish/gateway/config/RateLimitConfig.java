b6418282f042a4e3573275b3b64dd1c0d80d88d1
elvesfish <120352509@qq.com>
2019-04-22 17:59:13 +0800

    添加gateway网关

---
+++ gateway-server/src/main/java/com/elvesfish/gateway/config/RateLimitConfig.java
@@ -0,0 +1,51 @@
+package com.elvesfish.gateway.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * @author: elvesfish
+ * @date: 2019/4/22
+ */
+@Configuration
+public class RateLimitConfig {
+
+    /**
+     * IP限流
+     * 自定义限流标志的key，多个维度可以从这里入手
+     * exchange对象中获取服务ID、请求信息，用户信息等
+     */
+    @Bean
+    @Qualifier("ipKeyResolver")
+    KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+    }
+
+    /**
+     * 用户限流
+     * 使用这种方式限流，请求路径中必须携带userId参数
+     *
+     * @return
+     */
+//    @Bean
+//    @Qualifier("userKeyResolver")
+//    KeyResolver userKeyResolver() {
+//        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("userId"));
+//    }
+
+    /**
+     * 接口限流
+     * 获取请求地址的uri作为限流key
+     *
+     * @return
+     */
+//    @Bean
+//    @Qualifier("apiKeyResolver")
+//    KeyResolver apiKeyResolver() {
+//        return exchange -> Mono.just(exchange.getRequest().getPath().value());
+//    }
+}
